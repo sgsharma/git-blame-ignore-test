9c801d9d736d8541c426a9efd8636615d09991b1
Sasha Sharma <ssharma@patreon.com>
2020-09-30 13:49:34 -0700

    added test app and requirements

---
+++ hello.py
@@ -0,0 +1,27 @@
+import os
+
+import sentry_sdk
+from dotenv import load_dotenv
+from flask import Flask
+from sentry_sdk.integrations.flask import FlaskIntegration
+
+load_dotenv()
+
+SENTRY_DSN = os.getenv("SENTRY_DSN")
+sentry_sdk.init(
+    dsn=SENTRY_DSN,
+    integrations=[FlaskIntegration()],
+    traces_sample_rate=1.0
+)
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route('/debug-sentry')
+def trigger_error():
+    division_by_zero = 1 / 0
